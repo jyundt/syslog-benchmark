f6a139126475db93ff2746c86cfd22747bc8e833
Jacob Yundt <jyundt@gmail.com>
2014-12-23 15:39:55 -0500

    UDP is working, but not TCP.

--- syslog-benchmark.go
+++ syslog-benchmark.go
@@ -6,14 +6,17 @@ import(
 	"flag"
 	"log/syslog"
 	"fmt"
+	"strconv"
+	"math"
 )
 
 func main() {
 	//Define our test flags
 	host := flag.String("host","localhost","hostname of syslog server")
+	port := flag.Int("port",514,"port of syslog server")
 	proto := flag.String("proto","tcp","protocol of syslog server: tcp/udp")
 	msgs := flag.Int("msgs",-1,"number of messages to send, will be unlimited if not set")
-	tag := flag.String("tag ","","syslog message tag")
+	tag := flag.String("tag","syslog-benchmark","syslog message tag")
 
 	//Quick sanity check on flags
 	flag.Parse()
@@ -27,17 +30,21 @@ func main() {
 		os.Exit(1)
 	}
 
+
 	//Open a connection to our syslog server
-	syslogwriter, e := syslog.Dial(*proto,*host,syslog.LOG_ERR,*tag)
+	syslogwriter, e := syslog.Dial(*proto,*host + ":" + strconv.Itoa(*port),syslog.LOG_ERR,*tag)
 	if e == nil {
 		log.SetOutput(syslogwriter)
 	}
 
-
 	msg := 0
 	for {
 		msg++
-		fmt.Println("msg:",msg)
+		//fmt.Println("msg:",msg)
+		log.Print(msg)
+		if math.Mod(float64(msg),10000) == 0 {
+			fmt.Println(msg)
+		}
 		if *msgs == -1 { 
 			continue 
 		} else if msg >= *msgs {
